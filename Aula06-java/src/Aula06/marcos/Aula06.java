5666575d0784ca472ca7b22319d8fbff9ab555da
marcospaulo <marcospaulo@marcospaulo.carbomatriz>
2022-11-14 16:00:34 -0300

    Adicionando o Campo de Idade

--- Aula06-java/src/Aula06/marcos/Aula06.java
+++ Aula06-java/src/Aula06/marcos/Aula06.java
@@ -2,10 +2,15 @@ package Aula06.marcos;
 
 public class Aula06 {
 	
+	//Declaração dos Atributos
 	private String nome;
 	
 	private String sobrenome;
+	
+	private int idade;
 
+
+	//Declaração dos Métodos
 	public String getNome() {
 		return nome;
 	}
@@ -20,6 +25,14 @@ public class Aula06 {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 
 }
